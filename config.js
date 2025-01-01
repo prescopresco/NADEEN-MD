c649fa78b2e3dd7522bb74fc3f37264436b561ef
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2025-01-01 13:03:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "BD93naLB#ZOJpgLrI7GZKGHJa1d9bGjCtlk1NH48smC5Rt99XmW8", //put your session id
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 MODE: process.env.MODE || "public", //private,public,groups or inbox
