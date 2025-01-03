164ec509954a23c05a12f9ab479374c6ce01eee7
prescopresco <prescopresco2@gmail.com>
2025-01-03 15:43:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "QwIQyZBQ#c6W0rI7QVqeS0fSp8cWCZqH2BFkQAJnGEwPPvKDahGc", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "public", //private,public,groups or inbox
